faa192d41cacd45eeb36de3740e61f2d4271e30b
jakubdrewniak <drewniak.jakub@@gmail.com>
2025-03-11 20:20:48 +0100

    Minor refactor.

--- C02L02_inprompt.ts
+++ C02L02_inprompt.ts
@@ -3,8 +3,6 @@ import { config } from "dotenv";
 import { ChatOpenAI } from "@langchain/openai"
 import { HumanMessage, SystemMessage } from "@langchain/core/messages";
 
-
-
 const postToken = async () => {
   try {
     // Post to get the token
